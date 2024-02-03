d365faee83fc4f761c801f0f49cf94688cbe5449
Isaac <cookeisaac2@gmail.com>
2024-02-03 15:39:48 +0000

    Added macroquad support

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("Letter Game")]
+async fn main() {
+    loop {
+        clear_background(RED);
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+
+        next_frame().await
+    }
+}
